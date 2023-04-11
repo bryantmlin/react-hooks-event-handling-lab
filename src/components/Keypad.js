ceb47da1a899615fa1e21a2942d81d2615c18a56
Bryant Lin <bryantmlin@gmail.com>
2023-04-11 13:36:16 -0600

    a

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,21 @@
 // Code Keypad Component Here
+import React from "react";
+
+
 
 function Keypad (){
+    function handleInput(e) {
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input
+            type='password'
+            name='search'
+            onChange={handleInput}
+            />
+        </div>
     )
 }
 
